fd610fc936a5d6393de66fbaab0da03714a8b946
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-03-26 18:01:29 -0300

    feat new prompt and states for rag agent

--- agent/prompts.py
+++ agent/prompts.py
@@ -2,3 +2,12 @@
 SIMPLE_ASSISTANT_PROMPT = """
 You are a helpful assistant that answers questions.
 """
+
+RAG_SYSTEM_PROMPT = """
+You are a helpful assistant that answers questions based on the provided context.
+
+Context:
+{context}
+
+If user asks about something that is not in the context, act as a general assistant and try to answer the question based on your knowledge.
+"""
--- agent/states.py
+++ agent/states.py
@@ -1,6 +1,7 @@
-from typing import List, Annotated, Literal
+from typing import List, Annotated, Literal, Dict, Any
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
+from langchain_core.documents import Document
 
 class ModelConfiguration:
     model_id: str
@@ -8,6 +9,12 @@ class ModelConfiguration:
     reasoning_effort: Literal["low", "medium", "high"]
     think_mode: bool
     temperature: float
+
+class RetrieverConfiguration:
+    user_id: int
+    thread_id: str
+    top_k: int
+    include_sources: bool
 
 class GraphState(TypedDict):
     """
@@ -19,3 +26,5 @@ class GraphState(TypedDict):
 
     messages: Annotated[List, add_messages]
     llm_config: ModelConfiguration
+    retriever_config: RetrieverConfiguration
+    retrieved_documents: List[Document]
